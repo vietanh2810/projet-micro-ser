192049691c06ebf197e137caafa497326907399a
vietanh2810 <vietanhc12@gmail.com>
2023-07-13 10:54:57 +0200

    order: fix npm run prob

--- order/src/order/order.module.ts
+++ order/src/order/order.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { OrderService } from './order.service';
+import { OrderController } from './order.controller';
 
 @Module({
   controllers: [OrderController],
